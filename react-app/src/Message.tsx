cf0a3fd92230880f8c72a9855739c4bb815ea297
Bilguun <bilguunb@berkeley.edu>
2025-01-14 12:51:24 -0800

    created side list, very basic with no styling

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,7 @@
+// Make sure to use PascalCasing for all functions in React!
+function Message() {
+   
+    return <h1>Hello</h1> //This is JS XML though it looks like html
+}
+
+export default Message; 
